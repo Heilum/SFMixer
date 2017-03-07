91b3ff650d901804d49c6173df6b0d376a414c44
Jagie <chen_cwf@163.com>
2017-03-07 12:15:33 +0800

    Initial Commit

---
+++ SFMixer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SFMixer
+//
+//  Created by CHENWANFEI on 07/03/2017.
+//  Copyright © 2017 SwordFish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
